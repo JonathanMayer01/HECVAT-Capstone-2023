1b4b3378319c89228b7daa7f4cb3340c031c0165
JonathanMayer01 <jonathanmayer105@gmail.com>
2023-04-26 18:13:47 -0500

    Captstone 4/26

---
+++ HECVAT_Survery/HECVAT_Survery/Pages/Page2(Brett).cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HECVAT_Survery.Pages
+{
+    public class Page2_Brett_Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ HECVAT_Survery/HECVAT_Survery/Pages/Survery_1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HECVAT_Survery.Pages
+{
+    public class Survery_1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
